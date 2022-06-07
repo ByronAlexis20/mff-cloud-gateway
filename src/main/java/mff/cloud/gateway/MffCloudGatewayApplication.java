b1a422d090bbe04f5a894c8b503db8af767eae3f
ByronAlexis20 <byronalexisramirez20@gmail.com>
2022-06-07 18:03:58 -0500

    start project

---
+++ src/main/java/mff/cloud/gateway/MffCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package mff.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MffCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MffCloudGatewayApplication.class, args);
+	}
+
+}
