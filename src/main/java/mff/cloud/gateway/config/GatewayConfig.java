b1ce3476e16c8017a71d7307e3050c62715997dc
ByronAlexis20 <byronalexisramirez20@gmail.com>
2022-08-17 01:23:30 -0500

    configuraciones

--- src/main/java/mff/cloud/gateway/config/GatewayConfig.java
+++ src/main/java/mff/cloud/gateway/config/GatewayConfig.java
@@ -18,6 +18,7 @@ public class GatewayConfig {
 	public RouteLocator routes(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route("auth", r -> r.path("/api/auth/**").uri("http://localhost:4040/"))
+				.route("mff-seguridad-cliente", r -> r.path("/mff-seguridad/usuariocliente/**").uri("http://localhost:4041/"))
 				.route("mff-seguridad", r -> r.path("/mff-seguridad/**").filters(f -> f.filter(filter)).uri("http://localhost:4041/"))
 				.route("mff-administracion", r -> r.path("/mff-administracion/**").filters(f -> f.filter(filter)).uri("http://localhost:4042/"))
 				.route("error", r -> r.path("/error/**").uri("http://localhost:8080/")).build();
